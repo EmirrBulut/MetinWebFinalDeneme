3b8445cbd09601b07e19fd44b6fd950ad49881d8
Emir Bulut <emir.bulut@ogr.deu.edu.tr>
2020-05-20 15:33:01 +0300

    FirstTry

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MetinWebFinal
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Deniyorum github");
         }
     }
 }
