3659d3ed0b50bbea429e484e560c9f3dc58705b4
Emir Bulut <emir.bulut@ogr.deu.edu.tr>
2020-05-20 15:31:07 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetinWebFinal
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
