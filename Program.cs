29fbcdada6bd33f391bbec754bed3302cc97b78c
Emir Bulut <emir.bulut@ogr.deu.edu.tr>
2020-05-24 15:51:12 +0300

    RestoranNesneleri
    Restoran nesneleri olusturuldu ve her nesne split edilmis puanları ile dolduruldu.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,115 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace MetinWebFinal
 {
     class MainClass
     {
+        
+        
+        public static List<string> ReadDatas()// Okunan verileri split etmeden once bunun icinde toplayacagız.
+        {
+            List<string> puredatas = new List<string>();
+            string file_path = @"/Users/emirbulut/Desktop/Metin-WebFinal/MetinWebFinal/RestoranListesi.txt";
+            FileStream fs = new FileStream(file_path, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string datas = sr.ReadLine();
+            while (datas != null)
+            {
+                datas = sr.ReadLine();
+                puredatas.Add(datas);
+            }
+            return puredatas;
+
+
+        }
+
+        public static List<RestoranAttiribute> SplitData(List<string> parameterdata)
+        {
+            List<RestoranAttiribute> restorans = new List<RestoranAttiribute>();
+            for (int i = 0; i < 130; i++)
+            {
+                restorans.Add(new RestoranAttiribute());
+            }
+
+            string[] tempRestoransArray = parameterdata.ToArray();
+
+            for (int i = 0; i < 130; i++)
+            {
+                restorans[i].Restoran_kodu = tempRestoransArray[i].Split(',')[0]; // Restoran kodu ozelligi atılıyor.
+                if (tempRestoransArray[i].Split(',')[1] == "?") // Resotran Ortam puanı kontrol edılıyor 
+                {
+                    restorans[i].Ortam = Convert.ToInt16("0");
+                }
+                else
+                {
+                    restorans[i].Ortam = Convert.ToInt16(tempRestoransArray[i].Split(',')[1]);
+                }
+                if (tempRestoransArray[i].Split(',')[2] == "?")
+                {
+                    restorans[i].Ortam_temizligi = Convert.ToInt16("0");
+                }
+                else
+                {
+                    restorans[i].Ortam_temizligi = Convert.ToInt16(tempRestoransArray[i].Split(',')[2]);
+
+                }
+                if (tempRestoransArray[i].Split(',')[3] == "?")
+                {
+                    restorans[i].Yemek_kalitesi = Convert.ToInt16("0");
+                }
+                else
+                {
+                    restorans[i].Yemek_kalitesi = Convert.ToInt16(tempRestoransArray[i].Split(',')[3]);
+                }
+                if (tempRestoransArray[i].Split(',')[4] == "?")
+                {
+                    restorans[i].Hizmet_kalitesi = Convert.ToInt16("0");
+                }
+                else
+                {
+                    restorans[i].Hizmet_kalitesi = Convert.ToInt16(tempRestoransArray[i].Split(',')[4]);
+                }
+                if (tempRestoransArray[i].Split(',')[5]=="?")
+                {
+                    restorans[i].Fiyat_uygunlugu = Convert.ToInt16("0");
+                }
+                else
+                {
+                    restorans[i].Fiyat_uygunlugu = Convert.ToInt16(tempRestoransArray[i].Split(',')[5]);
+                }
+                if (tempRestoransArray[i].Split(',')[6] == "?")
+                {
+                    restorans[i].Ulasim_kolaylıgi = Convert.ToInt16("0");
+                }
+                else
+                {
+                    restorans[i].Ulasim_kolaylıgi = Convert.ToInt16(tempRestoransArray[i].Split(',')[6]);
+                }
+                if (tempRestoransArray[i].Split(',')[7] == "?")
+                {
+                    restorans[i].Araba_park_olanagi = Convert.ToInt16("0");
+                }
+                else
+                {
+                    restorans[i].Araba_park_olanagi = Convert.ToInt16(tempRestoransArray[i].Split(',')[7]);
+
+                }
+            }
+            return restorans;
+        }
+
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Deniyorum github");
-            Console.WriteLine("Simdi ne olacak?");
+            List<string> puredatas = ReadDatas(); // verileri okuttuk
+            List<RestoranAttiribute> restorans2 = SplitData(puredatas); // her bir elemanı,bir restoranı ve onun ozellıklerının puanlarını tutan liste. Suan restorans2nin icinde 130 restoran nesnesi var.
+            for (int i = 0; i < restorans2.Count; i++)
+            {
+                Console.WriteLine("Restoran Kodu : {0} - Ortam Temizligi : {1}",restorans2[i].Restoran_kodu,restorans2[i].Ortam_temizligi);
+            }
+            Console.WriteLine(restorans2.Count);
         }
     }
 }
---
+++ RestoranAttiribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace MetinWebFinal
+{
+    public class RestoranAttiribute
+    {
+        public RestoranAttiribute()
+        {
+        }
+
+        private string restoran_kodu;
+        private int ortam;
+        private int ortam_temizligi;
+        private int yemek_kalitesi;
+        private int hizmet_kalitesi;
+        private int fiyat_uygunlugu;
+        private int ulasim_kolaylıgi;
+        private int araba_park_olanagi;
+
+        public string Restoran_kodu { get => restoran_kodu; set => restoran_kodu = value; }
+        public int Ortam { get => ortam; set => ortam = value; }
+        public int Ortam_temizligi { get => ortam_temizligi; set => ortam_temizligi = value; }
+        public int Yemek_kalitesi { get => yemek_kalitesi; set => yemek_kalitesi = value; }
+        public int Hizmet_kalitesi { get => hizmet_kalitesi; set => hizmet_kalitesi = value; }
+        public int Fiyat_uygunlugu { get => fiyat_uygunlugu; set => fiyat_uygunlugu = value; }
+        public int Ulasim_kolaylıgi { get => ulasim_kolaylıgi; set => ulasim_kolaylıgi = value; }
+        public int Araba_park_olanagi { get => araba_park_olanagi; set => araba_park_olanagi = value; }
+    }
+}
